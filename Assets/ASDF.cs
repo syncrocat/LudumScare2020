249a713f98ccf507f754a2d23bbedeb31db8303d
Grant Zeidler <r.grant.zeidler@gmail.com>
2020-10-02 18:13:47 -0400

    Working Debugs :)

---
+++ Assets/ASDF.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ASDF : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnityEngine.Debug.Log("ASDF");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
