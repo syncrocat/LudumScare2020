bf4c3cf75d70f1ca285788aa2433ece2dc799739
Nick <nick.a.molinari@gmail.com>
2020-10-02 22:30:34 -0400

    Basic gamermanager architecture

---
+++ Assets/Managers/GamerManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public abstract class MiniGameManager :  MonoBehaviour
+{
+    public UnityAction<int> DoneGame;
+    public virtual void StartGame(float difficulty) { }
+    public virtual void DestroySelf() { Destroy(this.gameObject); }
+}
+
+[RequireComponent(typeof(HealthManager))]
+public class GamerManager : MonoBehaviour
+{
+    // Constants
+    private readonly float DIFFICULTY_SCALE_SPEED = 0.01f; // arbitrary units per second
+
+    // Variables
+    [SerializeField] private List<GameObject> m_games;
+    private HealthManager m_healthManager;
+
+    private GameObject m_currentGame;
+    private MiniGameManager m_currentGameManager;
+    private int m_currentGameIndex = -1;
+    [SerializeField] private float m_difficultyModifier = 0; //Only serialized for editor viewing purposes
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_healthManager = GetComponent<HealthManager>();
+        StartMiniGame();
+    }
+
+    void StartMiniGame()
+    {
+        //  Start new game stuff
+        m_currentGameIndex = PickNewGameIndex();
+        m_currentGame = Instantiate(m_games[m_currentGameIndex], new Vector3(0, 0, 0), Quaternion.identity);
+        m_currentGameManager = m_currentGame.GetComponent<MiniGameManager>();
+        m_currentGameManager.DoneGame += FinishMiniGame;
+        m_currentGameManager.StartGame(m_difficultyModifier);
+    }
+
+    void FinishMiniGame(int reward)
+    {
+        // End old game stuff
+        m_healthManager.AddHP(reward);
+        m_currentGameManager.DoneGame -= FinishMiniGame;
+        m_currentGameManager.DestroySelf();
+
+        // Start new
+        StartMiniGame();
+    }
+
+    private int PickNewGameIndex()
+    {
+        int maxIndex = m_games.Count;
+        if (m_currentGameIndex != -1 && m_games.Count > 1) maxIndex -= 1;
+        int gameIndex = Random.Range(0, maxIndex);
+
+        if  (m_currentGameIndex != -1 && m_games.Count > 1 && gameIndex >= m_currentGameIndex)
+        {
+            gameIndex += 1;
+        }
+
+        return gameIndex;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        m_difficultyModifier += DIFFICULTY_SCALE_SPEED * Time.deltaTime; 
+    }
+}
---
+++ Assets/Managers/HealthManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+    // Constants 
+    readonly float MAX_HEALTH = 100;
+    readonly float STANDARD_DRAIN_RATE = 5;  //in units per second
+    readonly float FEVER_DRAIN_RATE = 10;  //in units per second
+
+    // Variables
+    [SerializeField] private float m_health; //Only serialized for editor viewing purposes
+    private float m_drainRate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_health = MAX_HEALTH;
+        m_drainRate = STANDARD_DRAIN_RATE;
+    }
+
+    void FixedUpdate()
+    {
+        float tt = Time.deltaTime;
+
+        m_health -= m_drainRate * tt;
+        if (m_health < 0)
+            Die();
+    }
+
+    public void AddHP(float x)
+    {
+        m_health += x;
+        if (m_health > MAX_HEALTH)
+            m_health = MAX_HEALTH;
+    }
+
+    public void RemoveHP(float x)
+    {
+        m_health -= x;
+        if (m_health < 0)
+            Die();
+    }
+
+    public float GetHP()
+    {
+        return m_health;
+    }
+
+    private void Die()
+    {
+        Debug.Log("We died!");
+        // todo Death goes here
+    }
+}
