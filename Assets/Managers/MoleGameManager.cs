88676551ea6e6524164e34693c1a791817b8bdf4
Grant Zeidler <r.grant.zeidler@gmail.com>
2020-10-03 03:44:56 -0400

    Mole

--- Assets/Managers/MoleGameManager.cs
+++ Assets/Managers/MoleGameManager.cs
@@ -30,6 +30,20 @@ public class MoleGameManager : MiniGameManager
     {
         base.FixedUpdate();
 
+        if (Input.GetMouseButtonDown(0)) {
+            Vector3 touchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 touchPos2D = new Vector2(touchPos.x, touchPos.y);
+
+            RaycastHit2D hit = Physics2D.Raycast(touchPos2D, Vector2.zero);
+    
+            var moleHit = hit.collider?.gameObject?.GetComponent<Mole>();
+            if (moleHit == null) {
+                return;
+            }
+
+            TapMole(moleHit);
+        }
+
         // Check for touches on any moles
         foreach(Touch touch in Input.touches)
         {
